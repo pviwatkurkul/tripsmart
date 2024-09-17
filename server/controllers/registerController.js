e9d14ad1ace3ee1d6a50e83722017c37bb5941c2
pviwatkurkul <pviwatkurkul@ucsd.edu>
2024-09-16 17:05:53 -0700

    added server files

---
+++ server/controllers/registerController.js
@@ -0,0 +1,4 @@
+const usersDB ={
+    users:require('../model/user.json'),
+    setUsers: function (data) { this.users = data}
+}
--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from  "./routes/auth.routh.js";
 const app = express();
+
 dotenv.config();
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -12,6 +14,11 @@ app.use(cors());
 const PORT = process.env.PORT || 8080;
 const MONGO = process.env.CONNECTION_URL;
 
+app.get("/", (req, res) => {
+    res.send("Hello Word!");
+}); 
+
+app.use("/api/auth", authRoutes);
 mongoose.connect(MONGO)
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
     .catch((error) => console.error(error));
